4a699b99806fdbd337579a45d55da98a704afbf7
rahalrazika <rahalraika@gmail.com>
2021-07-30 20:33:51 +0200

    add patnership controller

---
+++ app/controllers/partnerships_controller.rb
@@ -0,0 +1,2 @@
+class PartnershipsController < ApplicationController
+end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
   private
 
   def set_project
-    @project = Project.find(params[:id])
+    @project = Project.find_by(id: params[:id])
   end
 
   def project_params
