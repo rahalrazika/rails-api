b7606104503400e53d9b8008d4e3226074490a65
rahalrazika <rahalraika@gmail.com>
2021-08-17 03:41:51 +0200

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,21 +10,21 @@ class ApplicationController < ActionController::API
   end
 
   def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1]
-      begin
-        JWT.decode(token, 'raza', true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        nil
-      end
+    return unless auth_header
+
+    token = auth_header.split(' ')[1]
+    begin
+      JWT.decode(token, 'raza', true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      nil
     end
   end
 
   def logged_in_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
+    return unless decoded_token
+
+    user_id = decoded_token[0]['user_id']
+    @user = User.find_by(id: user_id)
   end
 
   def logged_in?
